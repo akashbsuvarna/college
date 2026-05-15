32b2840c96ab0aa23acd488a913ce91079e2cc84
Akash B Suvarna <akashbsuvarna@gmail.com>
2026-05-15 17:46:14 +0530

    Fixed build script paths for Vercel

--- flutter_frontend/build.sh
+++ flutter_frontend/build.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 # Install Flutter in a local directory
-if [ ! -d "../flutter" ]; then
+if [ ! -d "flutter_sdk" ]; then
   echo "Cloning Flutter..."
-  git clone https://github.com/flutter/flutter.git -b stable ../flutter
+  git clone https://github.com/flutter/flutter.git -b stable flutter_sdk
 fi
 
 # Add Flutter to PATH
-export PATH="$PATH:$(pwd)/../flutter/bin"
+export PATH="$PATH:$(pwd)/flutter_sdk/bin"
 
 echo "Checking Flutter version..."
 flutter --version
+
+# Move to frontend directory
+cd flutter_frontend
 
 echo "Running flutter pub get..."
 flutter pub get
